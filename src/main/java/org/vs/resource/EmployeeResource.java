0875ab0d8fb6cbcfab9a598ddfe8ef48548ab694
Vishal Sinha <vishal.sinha21@gmail.com>
2020-06-28 16:24:10 +0200

    add 2 employees to map

--- src/main/java/org/vs/resource/EmployeeResource.java
+++ src/main/java/org/vs/resource/EmployeeResource.java
@@ -34,6 +34,8 @@ public class EmployeeResource {
         employees.put(1, emp1);
         employees.put(2, emp2);
         employees.put(3, emp3);
+        employees.put(4, emp4);
+        employees.put(5, emp5);
     }
 
     @RequestMapping(value = "/employees", method = RequestMethod.GET)
