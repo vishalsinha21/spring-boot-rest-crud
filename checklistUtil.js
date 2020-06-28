34b25d4b7d1e384992edfac6fee45c28a1becf4c
Vishal Sinha <vishal.sinha21@gmail.com>
2020-06-28 19:41:11 +0200

    check comment on PR

--- checklistUtil.js
+++ checklistUtil.js
@@ -55,4 +55,32 @@ fs.readFile(filePath, 'utf8', function (err,data) {
     }
   });
 
+  run(checklist)
+
 });
+
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run(message) {
+  try {
+    const github_token = core.getInput('GITHUB_TOKEN');
+
+    const context = github.context;
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.');
+      return;
+    }
+    const pull_request_number = context.payload.pull_request.number;
+
+    const octokit = new github.GitHub(github_token);
+    const new_comment = octokit.issues.createComment({
+      ...context.repo,
+      issue_number: pull_request_number,
+      body: message
+    });
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
